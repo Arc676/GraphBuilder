34621c74c24a3f107389f244968dcf1bda48bb17
Arc676/Alessandro Vinciguerra <alesvinciguerra@gmail.com>
2017-12-28 12:18:15 +0800

    Added GPL text
    Set app category
    Added Credits.rtf

--- GraphBuilder/ViewController.m
+++ GraphBuilder/ViewController.m
@@ -3,25 +3,24 @@
 //  GraphBuilder
 //
 //  Created by Alessandro Vinciguerra on 28/12/2017.
-//  Copyright © 2017 Arc676. All rights reserved.
-//
+//      <alesvinciguerra@gmail.com>
+//Copyright (C) 2017 Arc676/Alessandro Vinciguerra
+
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation (version 3)
+
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License for more details.
+
+//You should have received a copy of the GNU General Public License
+//along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//See README and LICENSE for more details
 
 #import "ViewController.h"
 
 @implementation ViewController
-
-- (void)viewDidLoad {
-	[super viewDidLoad];
-
-	// Do any additional setup after loading the view.
-}
-
-
-- (void)setRepresentedObject:(id)representedObject {
-	[super setRepresentedObject:representedObject];
-
-	// Update the view, if already loaded.
-}
-
 
 @end
